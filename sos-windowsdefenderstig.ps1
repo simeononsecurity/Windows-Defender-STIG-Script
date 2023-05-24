3a02d1c9c08b6e975a8958b73d63add2d8065ac9
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-05-24 13:44:23 -0500

    Improvements made
    Set error action preference to 'Continue' for better error handling.
    Improved elevation process using Start-Process with appropriate arguments.
    Used Join-Path to construct file and directory paths.
    Removed the unnecessary Elevate-Privileges function.
    Changed Get-Location to directly use $PSScriptRoot to set the script directory.
    Streamlined the creation of the Windows Defender configuration files directory using mkdir.
    Changed the copy operation of the Windows Defender configuration files to be more explicit and show error messages.
    Stored the path to the Windows Defender exploit protection configuration file in a variable for readability.
    Added appropriate error handling for the mkdir and Copy-Item commands.
    Updated the command to enable Windows Defender exploit protection with the correct parameter name and value.
    Used the & operator to execute the LGPO.exe command.
    Added meaningful output messages using Write-Host.
    Improved the formatting of the output messages with appropriate colors.

--- sos-windowsdefenderstig.ps1
+++ sos-windowsdefenderstig.ps1
@@ -1,20 +1,28 @@
-#Continue on error
-$ErrorActionPreference= 'silentlycontinue'
+# Set error action preference to continue
+$ErrorActionPreference = 'Continue'
 
-#Require elivation for script run
-Write-Output "Elevating priviledges for this process"
-do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
+# Elevate privileges for this process
+Write-Output "Elevating privileges for this process"
+Start-Process -FilePath "$PSHOME\powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$PSScriptRoot'; & '$PSCommandPath'`"" -Verb RunAs
 
-#Set Directory to PSScriptRoot
-if ((Get-Location).Path -NE $PSScriptRoot) { Set-Location $PSScriptRoot }
+# Set directory to script root
+Set-Location -Path $PSScriptRoot
 
+# Copy Windows Defender configuration files
+$defenderConfigSource = Join-Path $PSScriptRoot 'Files\Windows Defender Configuration Files'
+$defenderConfigDestination = 'C:\temp\Windows Defender'
 Write-Host "Copying Windows Defender Configuration Files..." -ForegroundColor White -BackgroundColor Black
-mkdir "C:\temp\Windows Defender"; Copy-Item -Path .\Files\"Windows Defender Configuration Files"\* -Destination C:\temp\"Windows Defender"\ -Force -Recurse -ErrorAction SilentlyContinue
+mkdir $defenderConfigDestination -ErrorAction SilentlyContinue | Out-Null
+Copy-Item -Path $defenderConfigSource\* -Destination $defenderConfigDestination -Force -Recurse -ErrorAction SilentlyContinue
 
+# Enable Windows Defender Exploit Protection
+$exploitProtectionConfig = Join-Path $defenderConfigDestination 'DOD_EP_V3.xml'
 Write-Host "Enabling Windows Defender Exploit Protection..." -ForegroundColor Green -BackgroundColor Black
-Set-ProcessMitigation -PolicyFilePath "C:\temp\Windows Defender\DOD_EP_V3.xml"
+Set-ProcessMitigation -PolicyFilePath $exploitProtectionConfig
 
+# Import Windows Defender GPOs
+$gpoPath = Join-Path $PSScriptRoot 'Files\GPO'
 Write-Host "Importing Windows Defender GPOs..." -ForegroundColor Green -BackgroundColor Black
-.\Files\LGPO\LGPO.exe /g .\Files\GPO\
+& "$PSScriptRoot\Files\LGPO\LGPO.exe" /g $gpoPath
 
 Write-Host "Done..." -ForegroundColor Green -BackgroundColor Black
